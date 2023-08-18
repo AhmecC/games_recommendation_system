b31b9ea92cdd5f912003cefc0034da2f02435699
Aqeel Ahmed <aqeelvahmed@gmail.com>
2023-08-18 17:41:10 +0100

    Update Metadata recommender.py

--- Metadata recommender.py
+++ Metadata recommender.py
@@ -3,7 +3,6 @@ import pandas as pd
 from ast import literal_eval
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity  
-from nltk.stem.snowball import SnowballStemmer
 
 games = pd.read_csv('games.csv')  # Games information
 users = pd.read_csv('users.csv')  # User_ID total games played/reviews information
@@ -49,7 +48,77 @@ clean_df.sort_values('wr', ascending=False).head(5)[['title','positive_ratio','w
 
 
 
-### -- Content Based Recommender -- ## 
+### -- Description Only based recommender -- ## 
+def get_recommendations(title):
+    idx = indices[title.lower()]  # Find location of game in dataframe
+    sim_scores = list(enumerate(cosine_sim[idx]))  # Find similarity with other games
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)  # List scores in descending order
+    sim_scores = sim_scores[1:31]
+    game_indices = [i[0] for i in sim_scores]  # Get id's
+    return titles.iloc[game_indices] # return top 30 most similar games
+
+tf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tf.fit_transform(clean_df['description'])
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)  # This measures similarity
+clean_df = clean_df.reset_index()  
+indices = pd.Series(clean_df.index, index=clean_df.title)  
+
+get_recommendations('call of duty').head(10)
+# ^ returns games it deems similar by what its description said
+
+
+
+
+### -- Metadata based recommender -- ## 
+clean_df['equal'] = clean_df.apply(lambda x: 'yes' if x['Developer'] == x['Publisher'] else 'no', axis=1)  # If Developer/Publisher are the same we don't include twice
+clean_df['Developer'] = clean_df['Developer'].apply(lambda x: str.lower(x.replace(" ", "")))
+clean_df['Developer'] = clean_df['Developer'].apply(lambda x: f'{x} {x} {x} {x} {x} {x} {x} {x}') # Increase Weight                                                   
+clean_df['Publisher'] = clean_df['Publisher'].apply(lambda x: str.lower(x.replace(" ", "")))                           
+clean_df['Publisher'] = clean_df['Publisher'].apply(lambda x: f'{x} {x} {x} {x} {x}')  # Increase Weight
+
+clean_df['tags'] = clean_df['tags'].apply(literal_eval) # Fix list 
+s = clean_df.apply(lambda x: pd.Series(x['tags']),axis=1).stack().reset_index(level=1, drop=True) # Take out tags from list
+s.name = 'tags'  
+s = s.value_counts()
+s = s[s > 1]  # includes all unique tags which are prevalent more than once
+
+def filter_tags(x):
+    words = []
+    for i in x:
+        if i in s:
+            words.append(i)
+    return words
+
+clean_df['tags'] = clean_df.tags.apply(filter_tags)
+clean_df['tags'] = clean_df.tags.apply(lambda x: [str.lower(i.replace(' ','')) for i in x])
+clean_df['tags'] = clean_df['tags'].apply(lambda x: ' '.join(x))  # Joins tags into one line
+clean_df['tags'] = clean_df['tags'].apply(lambda x: f'{x} {x} {x}')
+
+clean_df['soup'] = clean_df.apply(lambda x: x['description'] + x['tags'] + x['Developer'] + x['Publisher'] + x['free'] if x['equal'] == 'no' else x['description'] + x['tags'] + x['Developer'] + x['free'], axis=1)
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(clean_df.tags)  # Get
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+# Takes the titles
+indices = pd.Series(clean_df.index, index=clean_df.title)  
+
+get_recommendations('call of duty').head(10)
+
+
+
+
+def improved_recomendations(title):
+    idx = indices[title.lower()]  # Section same as get_recommendations
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:21]
+    game_indices = [i[0] for i in sim_scores]
+    
+    games_ = clean_df.iloc[game_indices][['title','year','positive_ratio','user_reviews', 'Developer', 'Publisher','wr']].sort_values('wr', ascending=False)
+        
+    return games_
+
 
 
 
