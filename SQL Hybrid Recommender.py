e02f6df7ad1ba9010c96cebd331489ab986b61d7
Aqeel <aqeelvahmed@gmail.com>
2023-09-17 12:04:49 +0100

    Update SQL Hybrid Recommender.py

--- SQL Hybrid Recommender.py
+++ SQL Hybrid Recommender.py
@@ -1,59 +1,66 @@
-# >>> This version uses SQL to improve speed
-import mysql.connector
+# >>> This version uses SQL to improve efficiency
 import numpy as np
 import pandas as pd
+import sqlite3
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 
-
-
 # ----- CONNECT TO MySQL DATABASE ----- #
-password = ''
-cnx = mysql.connector.connect(user = 'root', password = password, host = 'localhost', database='steam_data')
+cnx = sqlite3.connect('recommender.db')
 cursor = cnx.cursor()
 
 
 
+# ----- CREATING DATABASE TABLES ----- #
+cursor.execute("""CREATE TABLE games_data
+        (app_id INTEGER, year INTEGER, title TEXT, rating TEXT,
+        positive_ratio FLOAT, user_reviews INTEGER, description TEXT, tags TEXT,
+        Developer TEXT, Publisher TEXT, free TEXT, wr FLOAT, equal TEXT, soup TEXT)""")
+clean_df.to_sql('games_data', cnx, if_exists='append', index=False)  # Easily append df like this
+
+cursor.execute("""CREATE TABLE review_data
+        (app_id INTEGER, title TEXT, helpful INTEGER, funny INTEGER, is_recommended BOOLEAN, 
+        hours REAL, user_id INTEGER, review_id INTEGER, products INTEGER, reviews INTEGER)""")
+reviews_df.to_sql('review_data', cnx, if_exists='append', index=False)
+
+
+
 # ----- ADD PERSONAL GAMES ----- #
-def sql_personal_reviews(app_id):
+def add_reviews(app_id):
     cursor.execute(f"SELECT title FROM games_data WHERE app_id = {app_id}")
     title = cursor.fetchall()[0][0]
-    return (app_id, title, 0, 0, True, 5, 1, 0, 3, 3)
+    return (app_id, title, 0, 0, True, 5, 1, 0, 3, 3)  # returns correct format to add to table
 
 list_IDs = [214510, 945360, 400, 1593500, 292030, 990080, 1888930, 1245620, 285160, 629820, 1030840, 1030830, 271590, 1817190,
         356190, 391220, 393080, 1233570, 1238840, 1222140, 1174180, 782330, 379720, 1151640, 1238080, 1817070, 225540, 552520,
         517630, 447040]
 
 cursor.execute("SELECT app_id FROM games_data")
-ordering = [app_id[0] for app_id in cursor.fetchall()]
+ordering = [app_id[0] for app_id in cursor.fetchall()]  # Get list of all app_id's
 
-data = []
+data = []  # only contains games that exist
 for id in list_IDs:
     if id in clean_df['app_id'].values:
-        data.append(sql_personal_reviews(id))    
+        data.append(add_reviews(id)) 
 
-query = """INSERT INTO review_data (app_id, title, helpful, funny, is_recommended, hours, user_id, review_id, products, reviews)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-for row in data:
-    cursor.execute(query, row)  
-cnx.commit()
+data = pd.DataFrame(data, columns=['app_id', 'title', 'helpful', 'funny', 'is_recommended', 'hours', 'user_id', 'review_id', 'products', 'reviews'])
+data.to_sql('review_data', cnx, if_exists='append', index=False)  # Append personal games
 
 
 
 # ----- CALCULATE COSINE SIMILARITY ----- #
 cursor.execute('SELECT soup FROM games_data')
-data = [row[0] for row in cursor.fetchall()]
+soup = [row[0] for row in cursor.fetchall()]  # Soup contains description, tags, developer, publisher and if free!
 
 tfv = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tfv.fit_transform(soup) 
-
-cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)  # Similarity metric uses game metadata
 
 
 
 # ----- CREATE MATRIX ----- #
 og = pd.read_sql('SELECT user_id, app_id, is_recommended FROM review_data', con=cnx)
-og = og.pivot(index='user_id', columns='app_id', values='is_recommended').reset_index().fillna(0)
+og = og.pivot(index='user_id', columns='app_id', values='is_recommended').reset_index().fillna(0)  # If not played assumed not recommended
 
 cursor.execute("SELECT app_id FROM games_data WHERE app_id NOT IN (SELECT app_id FROM review_data)")
 app_ids = [app_id[0] for app_id in cursor.fetchall()]
