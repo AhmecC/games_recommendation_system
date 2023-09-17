cc31dcf63ac44510ce10a77341760377abdf255c
Aqeel <aqeelvahmed@gmail.com>
2023-09-17 17:41:44 +0100

    Update SQL Hybrid Recommender.py

--- SQL Hybrid Recommender.py
+++ SQL Hybrid Recommender.py
@@ -5,7 +5,7 @@ import sqlite3
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 
-# ----- CONNECT TO MySQL DATABASE ----- #
+# ----- CONNECT TO SQL DATABASE ----- #
 cnx = sqlite3.connect('recommender.db')
 cursor = cnx.cursor()
 
@@ -52,53 +52,55 @@ data.to_sql('review_data', cnx, if_exists='append', index=False)  # Append perso
 cursor.execute('SELECT soup FROM games_data')
 soup = [row[0] for row in cursor.fetchall()]  # Soup contains description, tags, developer, publisher and if free!
 
-tfv = TfidfVectorizer(stop_words='english')
+tfv = TfidfVectorizer(stop_words='english')  # Diminishes weight for commonly occuring words
 tfidf_matrix = tfv.fit_transform(soup) 
-cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)  # Similarity metric uses game metadata
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
 
 
 # ----- CREATE MATRIX ----- #
-og = pd.read_sql('SELECT user_id, app_id, is_recommended FROM review_data', con=cnx)
-og = og.pivot(index='user_id', columns='app_id', values='is_recommended').reset_index().fillna(0)  # If not played assumed not recommended
+user_recom = pd.read_sql('SELECT user_id, app_id, is_recommended FROM review_data', con=cnx)
+user_recom = user_recom.pivot(index='user_id', columns='app_id', values='is_recommended').reset_index().fillna(0)  # Assumed not recommended if not played
 
 cursor.execute("SELECT app_id FROM games_data WHERE app_id NOT IN (SELECT app_id FROM review_data)")
 app_ids = [app_id[0] for app_id in cursor.fetchall()]
 new_df = pd.DataFrame(0, index=range(5001), columns=app_ids)
-og = pd.concat([og, new_df], axis=1)  # Add missing ID's to reviews_df
+user_recom = pd.concat([user_recom, new_df], axis=1)  # 230 games are missing from user_recom which we add into the columns with 0's
 
 cursor.execute("SELECT app_id FROM games_data")
 ordering = [app_id[0] for app_id in cursor.fetchall()]
-og = og[['user_id'] + ordering]
+user_recom = user_recom[['user_id'] + ordering]  # Make sure that user_recom and Cosine sim have the same ordering
 
-og_items = og.drop('user_id', axis=1)
-og_matrix = pd.DataFrame(cosine_sim, index=ordering, columns=ordering)
+user_items = user_recom.drop('user_id', axis=1)
+cosine_sim = pd.DataFrame(cosine_sim, index=ordering, columns=ordering)
 
 
 
-# ----- HYBRID MATRIX ----- #
+# ----- HYBRID MATRIX (User-Item Collaborative Filtering ----- #
 def hybrid(user):
-    cursor.execute(f"SELECT app_id FROM review_data WHERE user_id = {user}")
-    known_games = [app_id[0] for app_id in cursor.fetchall()]
-    
-    user_index = og[og.user_id == user].index.to_list()[0]
-    user_rating_vector = og_items.iloc[user_index] 
+    cursor.execute(f"SELECT app_id FROM review_data WHERE user_id = {user)")
+    known_games = [app_id[0] for app_id in cursor.fetchall()]  # Games user has played
 
-    score = og_matrix.dot(user_rating_vector).div(og_matrix.sum(axis=1))
-    score = score.drop(known_games)    
-    score = score.nlargest(20)
+    user_index = user_recom[user_recom.user_id == user].index.to_lis()[0]
+    user_rating_vector = user_items.iloc[user_index]  # Vector of games recommended for specific user
+
+    score = cosine_sim.dot(user_rating_vector).div(cosine_sim.sum(axis=1))
+        # cosine_sim.dot(user_rating_vector) --> Returns weighted scores between user recommended games & all other games (Collaborative Filtering)
+        # div(cosine_sim.sum(axis=1)) --> Sum to get total similarity score for a game, then divide to normalise scores (weighted by overall similarity of each game) (Cosine Similarity)
+    score = score.drop(known_games)
+    score = score.nlargest(20)  # Top 20 most relevant games
     
-    app_ids = ','.join(map(str, score.index))
+    app_ids = ','.join(map(str, score.index))  # Get app_id for these games and list
     top_games = pd.read_sql(f'SELECT title, wr FROM games_data WHERE app_id IN ({app_ids})', con=cnx)
     top_games['relevance'] = score.values
-    top_games = top_games.head(10).sort_values('wr', ascending=False)
+    top_games = top_games.head(10).sort_values('wr', ascending=False)  # Rank top 10 by weighted rating
     
     known_games = ','.join(map(str, known_games))
-    known_games = pd.read_sql(f'SELECT title FROM games_data WHERE app_id IN ({known_games})', con=cnx).title
+    known_games = pd.read_sql(f'SELECT title FROM games_data WHERE app_id IN ({known_games})', con=cnx).title  # get all user recommended games
     return top_games, known_games
 
 user_id = 1
-hybrid(user_id)[0].head(10)
+hybrid(user_id)[0].head(10) 
 hybrid(user_id)[1]
 
 
